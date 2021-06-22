541843270f1479b06dba7680987bb5b25d0a199b
cristinaStratan1 <56672529+cristinaStratan1@users.noreply.github.com>
2021-06-22 22:44:09 +0200

    Update Account.java

--- AccountService/src/main/java/it/univaq/sose/train/account/Account.java
+++ AccountService/src/main/java/it/univaq/sose/train/account/Account.java
@@ -4,33 +4,63 @@ import java.sql.SQLException;
 import java.util.List;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import it.univaq.sose.train.booking.service.BookingModel;
 
 
 @Path("/account")
 public interface Account {
 	
-	@POST
-	@Path("/login")
-	@Produces({ MediaType.APPLICATION_JSON })
-	public AccountModel checkLogin(@QueryParam("uname") String username,@QueryParam("pass") String password)  throws SQLException, ClassNotFoundException;
+	@Operation(
+		       description = "The description of the getTodo operation goes here!",
+		       responses = {
+		          @ApiResponse(
+		             description = "The description of the Response goes here!", 
+		             content = @Content(mediaType = MediaType.APPLICATION_JSON, 
+		                                schema = @Schema(implementation = AccountModel.class)))
+		           }
+			)
 	
-	@POST
-	@Path("/register")
-	@Produces({ MediaType.APPLICATION_JSON })
-	int registerAccount(@QueryParam("firstname") String firstname, @QueryParam("lastname") String lastname, @QueryParam("username") String username, 
-			@QueryParam("password") String password, @QueryParam("age") int age, @QueryParam("gender") String gender, @QueryParam("address") String address) throws ClassNotFoundException;
+	@GET
+	@Path("{login}")
+	public AccountModel checkLogin(@PathParam(value ="uname") String username, @PathParam(value ="pass") String password)  throws SQLException, ClassNotFoundException;
 	
-	@POST
-	@Path("/userTickets")
-	@Produces({ MediaType.APPLICATION_JSON })
-	List<BookingModel> accountTickets(@QueryParam("userid") int userid) ;
-	int registerAccount(AccountModel account) throws ClassNotFoundException;
+	@Operation(
+		       description = "The description of the getTodo operation goes here!",
+		       responses = {
+		          @ApiResponse(
+		             description = "The description of the Response goes here!", 
+		             content = @Content(mediaType = MediaType.APPLICATION_JSON, 
+		                                schema = @Schema(implementation = int.class)))
+		           }
+			)
+	@GET
+	@Path("{register}")
+	int registerAccount(@PathParam(value ="firstname") String firstname, @PathParam(value ="lastname") String lastname, @PathParam(value ="username") String username, 
+			@PathParam(value ="password") String password, @PathParam(value ="age") int age, @PathParam(value ="gender") String gender, @PathParam(value ="address") String address) throws ClassNotFoundException;
+	
+	@Operation( 
+		       description = "The description of the getTodos operation goes here!",
+				responses = {
+			       @ApiResponse(
+			          description = "The description of the Response goes here!",
+			          content = {
+			              @Content(mediaType = MediaType.APPLICATION_JSON,
+			                 array = @ArraySchema(schema = @Schema(implementation = BookingModel.class)))
+			          }
+			       )
+			    }
+			 )
+	@GET
+	@Path("{userTickets}")
+	List<BookingModel> accountTickets(@PathParam(value ="userid") int userid) ;
 
 }
